52285e9247766285fd78bfe825614a7365c54951
axerex12 <axel.aaldijk12@gmail.com>
2025-04-15 13:17:40 +0300

    check username

--- login.js
+++ login.js
@@ -64,4 +64,23 @@ document.addEventListener('DOMContentLoaded', () => {
     loginModal.showModal();
   }
 
+  async function checkUserName(username) {
+    const response = await fetch(`https://media2.edu.metropolia.fi/restaurant/api/v1/users/available/:${username}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    console.log(response);
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || '');
+      }
+      const data = await response.json();
+      console.log(data);
+
+      return data;
+  }
+
 });
